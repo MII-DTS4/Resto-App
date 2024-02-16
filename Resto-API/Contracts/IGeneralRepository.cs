5e8c4e6465baced98e947817923f4dc3443ee529
Ahmad Yazid Munif <ahmadyazid76@gmail.com>
2024-02-16 10:32:34 +0700

    add General Repository and Contract

---
+++ Resto-API/Contracts/IGeneralRepository.cs
@@ -0,0 +1,14 @@
+﻿using Resto_API.Data;
+
+namespace Resto_API.Contracts
+{
+    public class IGeneralRepository<TEntity>
+    {
+        //RestoAppDbContext GetContext();
+        //IEnumerable<TEntity> GetAll();
+        //TEntity? GetByGuid(Guid guid);
+        //TEntity? Create(TEntity entity);
+        //bool Update(TEntity entity);
+        //bool Delete(TEntity entity);
+    }
+}
---
+++ Resto-API/Repositories/GeneralRepository.cs
@@ -0,0 +1,73 @@
+﻿using Resto_API.Contracts;
+using Resto_API.Data;
+
+namespace Resto_API.Repositories
+{
+    public class GeneralRepository<TEntity> : IGeneralRepository<TEntity> where TEntity : class
+    {
+        public readonly RestoAppDbContext _context;
+
+        protected GeneralRepository(RestoAppDbContext context)
+        {
+            _context = context;
+        }
+        public RestoAppDbContext GetContext()
+        {
+            return _context;
+        }
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public TEntity? GetByGuid(Guid guid)
+        {
+            var entity = _context.Set<TEntity>().Find(guid);
+            _context.ChangeTracker.Clear();
+            return entity;
+        }
+
+        public TEntity? Create(TEntity entity)
+        {
+            try
+            {
+                _context.Set<TEntity>().Add(entity);
+                _context.SaveChanges();
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool Update(TEntity entity)
+        {
+            try
+            {
+                _context.Set<TEntity>().Update(entity);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(TEntity entity)
+        {
+            try
+            {
+                _context.Set<TEntity>().Remove(entity);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+
+}
